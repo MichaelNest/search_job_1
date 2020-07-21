fdfebdf030dd10e98063fc5604423a6209ea685b
MichaelNest <nesterenkom385@gmail.com>
2020-07-21 16:22:30 +0300

    Add to model City class Meta, parametr verbose_name, function __str__. Add model City to admin.py.

--- scrap_app/models.py
+++ scrap_app/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 class City(models.Model):
-    name = models.CharField(max_length=55)
+    name = models.CharField(max_length=55, verbose_name='Название города')
     slug = models.SlugField(max_length=55, blank=True)
+    
+    class Meta:
+        verbose_name = 'Название населенного пункта'
+        verbose_name_plural = 'Название населенных пунктов'
+        
+    def __str__(self):
+        return self.name 
