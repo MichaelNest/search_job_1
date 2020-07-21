4b6803dc01cde45164d5efe45464865eee16f7f1
MichaelNest <nesterenkom385@gmail.com>
2020-07-21 17:03:47 +0300

    Add parametr unique=True to CharField in models.py. Create model Language. Register model Language in admin.py

--- scrap_app/models.py
+++ scrap_app/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
+
 class City(models.Model):
-    name = models.CharField(max_length=55, verbose_name='Название города')
+    name = models.CharField(max_length=55,
+                            verbose_name='Название города',
+                            unique=True)
     slug = models.SlugField(max_length=55, blank=True)
-    
+
     class Meta:
         verbose_name = 'Название населенного пункта'
         verbose_name_plural = 'Название населенных пунктов'
-        
+
     def __str__(self):
-        return self.name 
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=55,
+                            verbose_name='Язык программирования',
+                            unique=True)
+    slug = models.SlugField(max_length=55, blank=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
