6727123355209d418b1e89a96dbe0b2e51787e58
MichaelNest <nesterenkom385@gmail.com>
2020-07-21 16:39:45 +0300

    Add verbose_name for scrap_app - add it to apps.py. Add default_app_config to __init__.py

---
+++ scrap_app/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'scrap_app.apps.ScrapAppConfig'
--- scrap_app/apps.py
+++ scrap_app/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ScrapAppConfig(AppConfig):
     name = 'scrap_app'
+    verbose_name = 'Приложение по сбору вакансий Scrap_app'
