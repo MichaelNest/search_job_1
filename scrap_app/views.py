b42dc7245cb27c5391f31b1b9e251217f62e2dcd
MichaelNest <nesterenkom385@gmail.com>
2020-07-23 16:46:59 +0300

    Create form in home.html.

--- scrap_app/views.py
+++ scrap_app/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from .models import Vacantion
 
 def home_view(request):
-    qs = Vacantion.objects.all()
+    # print(request.GET)
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__name'] = city
+        if language:
+            _filter['language__name'] = language
+        qs = Vacantion.objects.filter(**_filter)
     context = {'object_list': qs}
     return render(request, 'scrap_app/home.html', context)
