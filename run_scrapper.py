369a8f280eec2bd8ba1594f9fd30052905786cae
MichaelNest <nesterenkom385@gmail.com>
2020-07-30 18:03:13 +0300

    Run run_scrapper.py, write parser-information to data-base.

--- run_scrapper.py
+++ run_scrapper.py
@@ -1,4 +1,3 @@
-from scrap_app.parser import *
 import codecs
 import os, sys
 
@@ -7,13 +6,17 @@ sys.path.append(proj)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'search_main.settings'
 import django
 django.setup()
+from django.db import DatabaseError
+from scrap_app.parser import *
+from scrap_app.models import City, Language, Vacantion
 
 parsers = ((work, 'https://www.work.ua/jobs-kyiv-python/'),
            (rabota, 'https://rabota.ua/jobsearch/vacancy_list?keyWords=Python&regionId=1'),
            (dou, 'https://jobs.dou.ua/vacancies/?city=%D0%A5%D0%B0%D1%80%D1%8C%D0%BA%D0%BE%D0%B2&category=Python'),
            (djinni, 'https://djinni.co/jobs/?location=%D0%9A%D0%B8%D0%B5%D0%B2&primary_keyword=Python')
            )
-
+city = City.objects.filter(slug='kiev').first()
+language = Language.objects.filter(slug='python').first()
 jobs, error = [], []
 
 for func, url in parsers:
@@ -21,12 +24,21 @@ for func, url in parsers:
     jobs.append(j)
     error.append(e)
 
-f = codecs.open('jobs.txt', 'w', 'utf-8')
-for a in jobs:
-    a.append('\n')
-    for b in a:
-        b = str(b)+'\n'
-    # a = str(a)+'\n'
-    # f.write(a)
-        f.write(b)
-f.close()
+for job in jobs:
+    for jb in job:
+        # if jb != '\n\n':
+        v = Vacantion(**jb, city=city, language=language)
+        try:
+            v.save()
+        except DatabaseError:
+            pass
+#
+# f = codecs.open('jobs.txt', 'w', 'utf-8')
+# for a in jobs:
+#     a.append('\n')
+#     for b in a:
+#         b = str(b)+'\n'
+#     # a = str(a)+'\n'
+#     # f.write(a)
+#         f.write(b)
+# f.close()
