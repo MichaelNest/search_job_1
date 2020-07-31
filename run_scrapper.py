0b5d83dca39ef5a0a7d85b1ad4c5d6e8479b0b1b
MichaelNest <nesterenkom385@gmail.com>
2020-07-31 17:18:51 +0300

    Create in run_scrapper.py func get_settings()

--- run_scrapper.py
+++ run_scrapper.py
@@ -1,6 +1,8 @@
 import codecs
 import os, sys
 
+from django.contrib.auth import get_user_model
+
 proj = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(proj)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'search_main.settings'
@@ -10,11 +12,19 @@ from django.db import DatabaseError
 from scrap_app.parser import *
 from scrap_app.models import City, Language, Vacantion, Error
 
+User = get_user_model()
+
 parsers = ((work, 'https://www.work.ua/jobs-kyiv-python/'),
            (rabota, 'https://rabota.ua/jobsearch/vacancy_list?keyWords=Python&regionId=1'),
            (dou, 'https://jobs.dou.ua/vacancies/?city=%D0%A5%D0%B0%D1%80%D1%8C%D0%BA%D0%BE%D0%B2&category=Python'),
            (djinni, 'https://djinni.co/jobs/?location=%D0%9A%D0%B8%D0%B5%D0%B2&primary_keyword=Python')
            )
+
+def get_settings():
+    qs = User.objects.filter(send_email=True).values()
+    settings_lst = set((q['city_id'], q['language_id']) for q in qs)
+    return settings_lst
+
 city = City.objects.filter(slug='kiev').first()
 language = Language.objects.filter(slug='python').first()
 jobs, error = [], []
